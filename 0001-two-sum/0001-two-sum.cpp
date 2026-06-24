5ad0c20731bfd492149448123f51dd017b7a6ed3
Sumit Saini <officialsumitsaini004@gmail.com>
2026-06-24 18:55:17 +0530

    Time: 3 ms (67.04%), Space: 14.9 MB (18.79%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+
+        for(int i = 0; i < nums.size(); i++) {
+
+            int need = target - nums[i];
+
+            if(mp.find(need) != mp.end()) {
+                return {mp[need], i};
+            }
+
+            mp[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
