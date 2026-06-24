31db92d977674bac26c2e45d45b083e3081d7816
Your Name <youremail@gmail.com>
2026-06-24 10:05:37 +0530

    Add automatic LeetCode organizer

--- organize.py
+++ organize.py
@@ -1,35 +1,67 @@
 import os
 import shutil
+import requests
 
-# Keywords -> Folder
+query = """
+query getQuestionDetail($titleSlug: String!) {
+  question(titleSlug: $titleSlug) {
+    topicTags {
+      slug
+    }
+  }
+}
+"""
+
 topic_map = {
-    "tree": "Trees",
-    "binary-tree": "Trees",
-    "bst": "BST",
-    "graph": "Graphs",
     "array": "Arrays",
     "string": "Strings",
+    "hash-table": "Hashing",
     "linked-list": "LinkedList",
     "stack": "Stack",
     "queue": "Queue",
-    "heap": "Heap",
-    "trie": "Tries",
     "binary-search": "BinarySearch",
-    "backtracking": "Backtracking",
+    "tree": "Trees",
+    "binary-search-tree": "BST",
+    "heap": "Heap",
+    "graph": "Graphs",
     "greedy": "Greedy",
+    "backtracking": "Backtracking",
     "dynamic-programming": "DynamicProgramming",
-    "bit-manipulation": "BitManipulation"
+    "bit-manipulation": "BitManipulation",
+    "trie": "Tries"
 }
 
-for item in os.listdir():
-    if os.path.isdir(item):
-        lower_name = item.lower()
+for folder in os.listdir():
+    if not os.path.isdir(folder):
+        continue
 
-        for key, folder in topic_map.items():
-            if key in lower_name:
-                destination = os.path.join(folder, item)
+    if folder in topic_map.values():
+        continue
 
-                if not os.path.exists(destination):
-                    shutil.move(item, destination)
-                    print(f"Moved {item} -> {folder}")
-                break
+    slug = folder.split("-",1)[-1]
+
+    try:
+        response = requests.post(
+            "https://leetcode.com/graphql",
+            json={
+                "query": query,
+                "variables": {"titleSlug": slug}
+            }
+        )
+
+        tags = response.json()["data"]["question"]["topicTags"]
+
+        if not tags:
+            continue
+
+        tag = tags[0]["slug"]
+
+        if tag in topic_map:
+            destination = os.path.join(topic_map[tag], folder)
+
+            if not os.path.exists(destination):
+                shutil.move(folder, destination)
+                print(folder, "->", topic_map[tag])
+
+    except:
+        pass
