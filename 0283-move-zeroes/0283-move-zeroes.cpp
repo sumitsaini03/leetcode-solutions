9ba7aabd6b2f4ab5c65d983b3e52e6e41c93d5a1
Sumit Saini <officialsumitsaini004@gmail.com>
2026-07-04 09:55:58 +0530

    Time: 0 ms (100%), Space: 23.8 MB (55.67%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;  // Position for next non-zero
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
