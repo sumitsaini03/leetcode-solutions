76b7a445b53da2bedf33877f0486971478677ca8
Sumit Saini <officialsumitsaini004@gmail.com>
2026-07-09 10:15:09 +0530

    Time: 0 ms (100%), Space: 24 MB (62.62%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -0,0 +1,25 @@
+class NumArray {
+public:
+    vector<int> prefix;
+
+    NumArray(vector<int>& nums) {
+        prefix.resize(nums.size());
+
+        prefix[0] = nums[0];
+
+        for(int i = 1; i < nums.size(); i++)
+            prefix[i] = prefix[i - 1] + nums[i];
+    }
+
+    int sumRange(int left, int right) {
+
+        if(left == 0)
+            return prefix[right];
+
+        return prefix[right] - prefix[left - 1];
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
