7023612f45b111eb891ddb90cfcf940d15b4b1a0
Your Name <youremail@gmail.com>
2026-07-16 18:14:06 +0530

    fix: organize.py

--- organize.py
+++ organize.py
@@ -160,4 +160,9 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        import traceback
+        traceback.print_exc()
+        raise
